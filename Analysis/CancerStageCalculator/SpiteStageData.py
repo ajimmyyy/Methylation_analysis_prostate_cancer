eaf3057d861626e3fb8ed4fed51e9413441edbdd
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2024-04-13 21:21:47 +0800

    feat-區分期數資料

--- Analysis/CancerStageCalculator/SpiteStageData.py
+++ Analysis/CancerStageCalculator/SpiteStageData.py
@@ -11,10 +11,32 @@ if __name__ == "__main__":
     _sampleSheetDf = pd.read_csv(_config["Paths"]["TCGA_PARD_SAMPLE_SHEET"])
 
     _cancerStageCalculator = CancerStageCalculator()
-
     _earlyDf = _cancerStageCalculator.CancerStageSplit(_betaDf, _sampleSheetDf, ["T1", "T2"])
     _nromalDf = _betaDf.iloc[:, :51]
 
     _mergedDf = pd.concat([_nromalDf, _earlyDf], axis=1)
 
     _mergedDf.to_csv(_config["Paths"]["EARLY_STAGE_DATA_PATH"], index=False)
+
+    # _betaDf = pd.read_csv(_config["Paths"]["TCGA_PARD_450K"])
+    # _betaDf2 = pd.read_csv(_config["Paths"]["BETA_DATA_PATH"])
+    # _betaDf2.columns.values[0] = "CpG"
+    # _sampleSheetDf = pd.read_csv(_config["Paths"]["TCGA_PARD_SAMPLE_SHEET"])
+    # _sampleSheetDf2 = pd.read_csv(_config["Paths"]["SAMPLE_SHEET_PATH"])
+
+    # _name = _betaDf.iloc[:, :1]
+    # _betaDf = _betaDf.iloc[:, 51:]
+    # _cancerStageCalculator = CancerStageCalculator()
+    # _earlyDf = _cancerStageCalculator.CancerStageSplit(_betaDf, _sampleSheetDf, ["T2"])
+
+    # _name2 = _betaDf2.iloc[:, :1]
+    # _betaDf2 = _betaDf2.iloc[:, 26:]
+    # _earlyDf2 = _cancerStageCalculator.CancerStageSplit(_betaDf2, _sampleSheetDf2, ["T2"], stageColName="ajcc_pathologic_t", sampleColName="sample_id")
+
+    # _mergedDf = pd.concat([_name, _earlyDf], axis=1)
+    # _mergedDf2 = pd.concat([_name2, _earlyDf2], axis=1)
+
+    # _earlyMerge = pd.merge(_mergedDf, _mergedDf2, on="CpG", how="outer")
+    # _earlyMerge.to_csv(_config["Paths"]["T2_STAGE_DATA_PATH"], index=False)
+
+    
--- Models/CancerStageCalculator.py
+++ Models/CancerStageCalculator.py
@@ -5,45 +5,14 @@ from tqdm import tqdm
 import re
 
 class CancerStageCalculator:
-    def MappingPatientTNM(self, cancerStageDf, IdDf):
-        filtered_data = cancerStageDf[cancerStageDf['TCGA_barcode'].isin(IdDf['TCGA_barcode'])]
-        df = pd.merge(IdDf, filtered_data, on = ["TCGA_barcode"], how = "outer")
-        return df
-    
-    def MarkCancerStage(self, sampleSheet):
-        tqdm.pandas(desc="find cancer stage")
-        sampleSheet["cancer_stage"] = sampleSheet.progress_apply(self.__JudgeCancerStage, axis = 1)
-        earlyPatient = sampleSheet[sampleSheet["cancer_stage"] == "early"].drop_duplicates()
-        laterPatient = sampleSheet[sampleSheet["cancer_stage"] == "later"].drop_duplicates()
+    def CancerStageSplit(self, allBetaDf: pd.DataFrame, sampleSheetDf: pd.DataFrame, cancerStage, stageColName = "clinical_T", sampleColName = "sample"):
+        sampleSheetDf[stageColName].fillna('Unknown', inplace=True)
 
-        return sampleSheet, earlyPatient, laterPatient
-    
-    def DrawDbetaDifference(self, dbataDifDf, binWidth, boundary, dbetaDifColumn = "dbate_dif"):
-        fig = plt.figure(figsize=(8,6))
-        std = dbataDifDf[dbetaDifColumn].std()
-        mean = dbataDifDf[dbetaDifColumn].mean()
+        if isinstance(cancerStage, str):
+            cancerStage = [cancerStage]
 
-        hist, bins = np.histogram(dbataDifDf[dbetaDifColumn], bins = np.arange(-boundary, boundary + binWidth, binWidth))
-        
-        colors = ['gray' if (x < (mean - 2 * std) or x > (mean + 2 * std)) else 'blue' for x in np.arange(-boundary, boundary + binWidth, binWidth)]
+        filteredSamples = sampleSheetDf[sampleSheetDf[stageColName].str.startswith(tuple(cancerStage))][sampleColName]
+        filteredSamples = filteredSamples.astype(str)
+        stageDf = allBetaDf.loc[:, allBetaDf.columns.isin(filteredSamples)]
 
-        plt.bar(bins[:-1], hist, width=binWidth, align='edge', color = colors)
-        plt.xlabel('delta bate difference')
-        plt.ylabel('times frequency')
-
-        return fig
-    
-    def __JudgeCancerStage(self, row):
-        if row["Sample_Group"] == "Normal":
-            return pd.Series({"cancer_stage": "None"})
-        
-        tumor_stage = str(row["tumor_stage"])
-        node_stage = str(row["node_stage"])
-
-        if  node_stage == "N0":
-            if re.search(r"T1|T2", tumor_stage):
-                return pd.Series({"cancer_stage": "early"})
-            else:
-                return pd.Series({"cancer_stage": "later"})
-        else:
-            return pd.Series({"cancer_stage": "later"})
+        return stageDf
