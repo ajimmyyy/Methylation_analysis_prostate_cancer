1a7ee1002ffd5711510fedb4712cd0b8eed1fe47
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2024-05-14 22:34:57 +0800

    feat-完成前後期beta分布分析

--- Analysis/TestCancerStage/TestCancerStage.py
+++ Analysis/TestCancerStage/TestCancerStage.py
@@ -4,23 +4,27 @@ import matplotlib.pyplot as plt
 from Models.DeltaBetaFilter import DeltaBetaFilter
 from MakeFile.FileSaver import FileSaver
 
-def DrawDiffBeta(earlyDf,laterDf):
+def DrawDiffBeta(normalDf, earlyDf, laterDf):
+    normalDf.set_index(normalDf.columns[0], inplace=True)
     earlyDf.set_index(earlyDf.columns[0], inplace=True)
     laterDf.set_index(laterDf.columns[0], inplace=True)
+    normalDf = normalDf.T
     earlyDf = earlyDf.T
     laterDf = laterDf.T
     earlyDf = earlyDf[75:]
     laterDf = laterDf[75:]
 
     fig, ax = plt.subplots()
-    box_width = 0.35
-    positions1 = range(len(earlyDf.columns))
+    box_width = 0.3
+    positions1 = range(len(normalDf.columns))
     positions2 = [x + box_width for x in positions1]
+    positions3 = [x + box_width * 2 for x in positions1]
 
-    earlyDf.boxplot(ax=ax, positions=positions1, widths=box_width, showfliers=False, patch_artist=True, color='skyblue')
-    laterDf.boxplot(ax=ax, positions=positions2, widths=box_width, showfliers=False, patch_artist=True, color='salmon')
+    normalDf.boxplot(ax=ax, positions=positions1, widths=box_width, showfliers=False, patch_artist=True, color='pink')
+    earlyDf.boxplot(ax=ax, positions=positions2, widths=box_width, showfliers=False, patch_artist=True, color='skyblue')
+    laterDf.boxplot(ax=ax, positions=positions3, widths=box_width, showfliers=False, patch_artist=True, color='salmon')
 
-    ax.set_xticks([p + box_width / 2 for p in positions1])
+    ax.set_xticks([p + box_width for p in positions1])
     ax.set_xticklabels(earlyDf.columns.tolist())
     ax.set_title('Early vs Late Stage Cancer')
     ax.legend()
@@ -53,21 +57,22 @@ if __name__ == "__main__":
 
     filterGene = ["GAPDH", "ACTB", "MYH14", "PRHOXNB"]
     _dmpDf = pd.read_csv(_config["Paths"]["DMP_DATA"], usecols=["CpG", "gene"])
+    _normalDf = pd.read_csv(_config["Paths"]["NORMAL_STAGE_DATA"])
     _earlyDf = pd.read_csv(_config["Paths"]["EARLY_STAGE_DATA"])
     _laterDf = pd.read_csv(_config["Paths"]["LATE_STAGE_DATA"])
-
-    # df = CalculateDeltaBeta(_earlyDf, _laterDf)
-    # FileSaver.SaveData(df, "C:/Users/acer/Desktop/test/test.csv")
+    _normalDf.columns.values[0] = "CpG"
+    _earlyDf.columns.values[0] = "CpG"
+    _laterDf.columns.values[0] = "CpG"
 
     for gene in filterGene:
         filerCpG = _dmpDf[_dmpDf["gene"] == gene]["CpG"].tolist()
+        normalDf = _normalDf[_normalDf["CpG"].isin(filerCpG)]
         earlyDf = _earlyDf[_earlyDf["CpG"].isin(filerCpG)]
         laterDf = _laterDf[_laterDf["CpG"].isin(filerCpG)]
 
         print("Gene:", gene)
-        CalculateDeltaBeta(earlyDf, laterDf)
-        DrawDiffBeta(earlyDf,laterDf)
-
+        df = CalculateDeltaBeta(earlyDf, laterDf)
+        DrawDiffBeta(normalDf, earlyDf, laterDf)
 
 
 
